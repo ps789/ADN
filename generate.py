511ad6ce8f0ba212aeec51c0d567d1a235b1cd0e
alexyu66 <alexyu66@gmail.com>
2023-03-15 14:33:45 -0400

    Generate conditional eval images

--- generate.py
+++ generate.py
@@ -1,13 +1,11 @@
 import torch
 from tqdm import tqdm
 from torchvision.utils import save_image
-from PIL import Image
 from tensorfn import load_config
-
-from model import UNet
 from config import DiffusionConfig
 from diffusion import GaussianDiffusion, make_beta_schedule
 import numpy as np
+import pickle
 
 @torch.no_grad()
 def p_sample_loop(self, model, noise, class_num, device, noise_fn=torch.randn, capture_every=1000):
@@ -27,23 +25,38 @@ def p_sample_loop(self, model, noise, class_num, device, noise_fn=torch.randn, c
     return imgs
 
 if __name__ == "__main__":
-    conf = load_config(DiffusionConfig, "config/conditional_diffusion.conf", show=False)
-    ckpt = torch.load("checkpoint/conditional_diffusion/conditional_diffusion_330000.pt")
-    model = conf.model.make()
-    model.load_state_dict(ckpt["ema"])
-    model = model.to("cuda")
-    betas = conf.diffusion.beta_schedule.make()
-    diffusion = GaussianDiffusion(betas).to("cuda")
 
-    class_num = 0
-    large_batch = False
-    
-    if large_batch:
-        for i in range(50):
-            noise = torch.randn([200, 3, 32, 32], device="cuda")
-            imgs = p_sample_loop(diffusion, model, noise, class_num, "cuda", capture_every=1000)
-            np.save(f"eval/conditional_diffusion/conditional_diffusion_samples_{i}.npy", imgs[-1].detach().cpu().numpy())
-    else:
-        noise = torch.randn([64, 3, 32, 32], device="cuda")
-        imgs = p_sample_loop(diffusion, model, noise, class_num, "cuda", capture_every=1000)
-        save_image(imgs[-1], "sample.png", normalize=False, range_value=(-1, 1), nrow=8)
+    model_names = ['conditional_diffusion_{0}'.format(str(x).zfill(6)) for x in np.arange(11) * 50000]
+
+    for model_name in model_names:
+
+        conf = load_config(DiffusionConfig, "config/conditional_diffusion.conf", show=False)
+        ckpt = torch.load("checkpoint/conditional_diffusion/{0}.pt".format(model_name))
+        model = conf.model.make()
+        model.load_state_dict(ckpt["ema"])
+        model = model.to("cuda")
+        betas = conf.diffusion.beta_schedule.make()
+        diffusion = GaussianDiffusion(betas).to("cuda")
+        
+        # Generate images
+        num_classes = 10
+        num_batches = 2
+        batch_size = 500
+        all_imgs = {}
+        for c in range(num_classes):
+
+            all_imgs[c] = np.zeros((num_batches * batch_size, 3, 32, 32))
+            for i in range(num_batches):
+                noise = torch.randn([batch_size, 3, 32, 32], device="cuda")
+                imgs = p_sample_loop(diffusion, model, noise, c, "cuda", capture_every=1000)
+                all_imgs[c][i * batch_size : (i + 1) * batch_size, ...] = imgs[-1].detach().cpu().numpy()
+
+        # Save images
+        with open('eval/conditional_diffusion/imgs_{0}.pickle'.format(model_name), 'wb') as p:
+            pickle.dump(all_imgs, p)
+
+    # Generate a grid of images:
+    # class_num = 0
+    # noise = torch.randn([64, 3, 32, 32], device="cuda")
+    # imgs = p_sample_loop(diffusion, model, noise, class_num, "cuda", capture_every=1000)
+    # save_image(imgs[-1], "sample.png", normalize=False, range_value=(-1, 1), nrow=8)
